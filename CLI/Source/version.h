80818f9846fb224764cff9dbe855c264c1ef0b17
Antoine GOHIN <contact@brokestudio.fr>
2026-09-02 10:50:17 +0200

    v0.0.2

--- CLI/Source/version.h
+++ CLI/Source/version.h
@@ -1,2 +1,2 @@
 #pragma once
-#define INLRETRO_CLI_VERSION "0.0.1"
+#define INLRETRO_CLI_VERSION "0.0.2"
--- GUI/Source/version.h
+++ GUI/Source/version.h
@@ -1,2 +1,2 @@
 #pragma once
-#define INLRETRO_GUI_VERSION "0.0.1"
+#define INLRETRO_GUI_VERSION "0.0.2"
