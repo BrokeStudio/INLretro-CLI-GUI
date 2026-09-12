ce2dd545d0a049ce0d30c326032784ed723fdd2c
Antoine GOHIN <contact@brokestudio.fr>
2026-09-12 23:36:23 +0200

    firmware: NES/GB bugfixes + code cleaning

--- firmware/firmware/source/flash.c
+++ firmware/firmware/source/flash.c
@@ -86,6 +86,45 @@ uint8_t nes_write_page_verify(uint8_t addrH, buffer* buff, write_rv_funcptr wr_f
   return SUCCESS;
 }
 
+uint8_t nes_cpu_write_page_buffer_verify(uint8_t addrH, buffer* buff)
+{
+  uint16_t cur = buff->cur_byte; // need 16 bits here so it won't overflow
+  uint16_t base_addr = (uint16_t)addrH << 8;
+  uint16_t addr;
+  uint8_t value;
+  uint16_t byte_count = (buff->last_idx + 1 - buff->cur_byte);
+
+  // write "write to buffer" command and sector address
+  nes_cpu_wr(0x8AAA, 0xAA);
+  nes_cpu_wr(0x8555, 0x55);
+  nes_cpu_wr(base_addr, 0x25);
+  nes_cpu_wr(base_addr, byte_count - 1);
+
+  while(cur <= buff->last_idx) {
+    value = buff->data[cur + 0];
+    addr = base_addr + cur;
+
+    // add word to write buffer
+    nes_cpu_wr(addr, value);
+
+    cur++;
+  }
+  buff->cur_byte = cur;
+
+  // write program buffer to flash (confirm)
+  nes_cpu_wr(base_addr, 0x29);
+
+  // TODO: add timeout
+  do {
+    value = nes_cpu_rd(addr);
+  } while(value != nes_cpu_rd(addr));
+
+  // TODO: add flash control
+
+  // TODO error check/report
+  return SUCCESS;
+}
+
 uint8_t nes_ppu_write_page_buffer_verify(uint8_t addrH, buffer* buff)
 {
   uint16_t cur = buff->cur_byte; // need 16 bits here so it won't overflow
@@ -290,86 +329,86 @@ uint8_t write_page_a53(uint8_t bank, uint8_t addrH, buffer* buff, write_funcptr 
   return SUCCESS;
 }
 
-uint8_t write_page_tssop(uint8_t bank, uint8_t addrH, buffer* buff, write_funcptr wr_func, read_funcptr rd_func)
-{
-  uint16_t cur = buff->cur_byte;
-  uint8_t n = buff->cur_byte;
-  uint8_t read;
-  // enter unlock bypass mode
-  wr_func(0x8AAA, 0xAA);
-  wr_func(0x8555, 0x55);
-  wr_func(0x8AAA, 0x20);
-  while(cur <= buff->last_idx) {
-    // curaddresswrite( 0xA0 );  //gained ~3KBps (59.13KBps) inl6 with v3.0 proto
-    wr_func(((addrH << 8) | n), 0xA0);
-    wr_func(((addrH << 8) | n), buff->data[n]);
-    do {
-      usbPoll();
-      read = rd_func((addrH << 8) | n);
-    } while(read != rd_func((addrH << 8) | n));
-    // retry if write failed
-    // this helped but still seeing similar fails to dumps
-    if(read == buff->data[n]) {
-      n++;
-      cur++;
-      LED_IP_PU();
-      LED_LO();
-    } else {
-      // kaz6 final needs a retry, but proto doesn't...
-      //  nes_cpu_wr(0x5000, 0x81); //outer reg select mode
-      //  nes_cpu_wr(0x8000, bank);    //outer bank
-      //  nes_cpu_wr(0x5000, 0x54); //chr reg select act like CNROM & enable flash writes
-      LED_OP();
-      LED_HI();
-    }
-  }
-  buff->cur_byte = n;
-  // exit unlock bypass mode
-  wr_func(0x8000, 0x90);
-  wr_func(0x8000, 0x00);
-  // reset the flash chip, supposed to exit too
-  wr_func(0x8000, 0xF0);
-  return SUCCESS;
-}
+// uint8_t write_page_tssop(uint8_t bank, uint8_t addrH, buffer* buff, write_funcptr wr_func, read_funcptr rd_func)
+// {
+//   uint16_t cur = buff->cur_byte;
+//   uint8_t n = buff->cur_byte;
+//   uint8_t read;
+//   // enter unlock bypass mode
+//   wr_func(0x8AAA, 0xAA);
+//   wr_func(0x8555, 0x55);
+//   wr_func(0x8AAA, 0x20);
+//   while(cur <= buff->last_idx) {
+//     // curaddresswrite( 0xA0 );  //gained ~3KBps (59.13KBps) inl6 with v3.0 proto
+//     wr_func(((addrH << 8) | n), 0xA0);
+//     wr_func(((addrH << 8) | n), buff->data[n]);
+//     do {
+//       usbPoll();
+//       read = rd_func((addrH << 8) | n);
+//     } while(read != rd_func((addrH << 8) | n));
+//     // retry if write failed
+//     // this helped but still seeing similar fails to dumps
+//     if(read == buff->data[n]) {
+//       n++;
+//       cur++;
+//       LED_IP_PU();
+//       LED_LO();
+//     } else {
+//       // kaz6 final needs a retry, but proto doesn't...
+//       //  nes_cpu_wr(0x5000, 0x81); //outer reg select mode
+//       //  nes_cpu_wr(0x8000, bank);    //outer bank
+//       //  nes_cpu_wr(0x5000, 0x54); //chr reg select act like CNROM & enable flash writes
+//       LED_OP();
+//       LED_HI();
+//     }
+//   }
+//   buff->cur_byte = n;
+//   // exit unlock bypass mode
+//   wr_func(0x8000, 0x90);
+//   wr_func(0x8000, 0x00);
+//   // reset the flash chip, supposed to exit too
+//   wr_func(0x8000, 0xF0);
+//   return SUCCESS;
+// }
 
-uint8_t nes_write_page_dualport(uint8_t bank, uint8_t addrH, buffer* buff, write_funcptr wr_func, read_funcptr rd_func)
-{
-  uint16_t cur = buff->cur_byte;
-  uint8_t n = buff->cur_byte;
-  uint8_t read;
-  // enter unlock bypass mode
-  wr_func(0x0AAA, 0xAA);
-  wr_func(0x0555, 0x55);
-  wr_func(0x0AAA, 0x20);
-  while(cur <= buff->last_idx) {
-    wr_func(((addrH << 8) | n), 0xA0);
-    wr_func(((addrH << 8) | n), buff->data[n]);
-    do {
-      usbPoll();
-      read = rd_func((addrH << 8) | n);
-    } while(read != rd_func((addrH << 8) | n));
-    // TODO verify byte is value that was trying to be flashed
-    // move on to next byte
-    // n++;
-    // cur++;
-    if(read == buff->data[n]) {
-      n++;
-      cur++;
-      LED_IP_PU();
-      LED_LO();
-    } else {
-      LED_OP();
-      LED_HI();
-    }
-  }
-  buff->cur_byte = n;
-  // exit unlock bypass mode
-  wr_func(0x0000, 0x90);
-  wr_func(0x0000, 0x00);
-  // reset the flash chip, supposed to exit too
-  wr_func(0x0000, 0xF0);
-  return SUCCESS;
-}
+// uint8_t nes_write_page_dualport(uint8_t bank, uint8_t addrH, buffer* buff, write_funcptr wr_func, read_funcptr rd_func)
+// {
+//   uint16_t cur = buff->cur_byte;
+//   uint8_t n = buff->cur_byte;
+//   uint8_t read;
+//   // enter unlock bypass mode
+//   wr_func(0x0AAA, 0xAA);
+//   wr_func(0x0555, 0x55);
+//   wr_func(0x0AAA, 0x20);
+//   while(cur <= buff->last_idx) {
+//     wr_func(((addrH << 8) | n), 0xA0);
+//     wr_func(((addrH << 8) | n), buff->data[n]);
+//     do {
+//       usbPoll();
+//       read = rd_func((addrH << 8) | n);
+//     } while(read != rd_func((addrH << 8) | n));
+//     // TODO verify byte is value that was trying to be flashed
+//     // move on to next byte
+//     // n++;
+//     // cur++;
+//     if(read == buff->data[n]) {
+//       n++;
+//       cur++;
+//       LED_IP_PU();
+//       LED_LO();
+//     } else {
+//       LED_OP();
+//       LED_HI();
+//     }
+//   }
+//   buff->cur_byte = n;
+//   // exit unlock bypass mode
+//   wr_func(0x0000, 0x90);
+//   wr_func(0x0000, 0x00);
+//   // reset the flash chip, supposed to exit too
+//   wr_func(0x0000, 0xF0);
+//   return SUCCESS;
+// }
 
 // #define PRGM_MODE() swim_wotf(SWIM_HS, 0x500F, 0x40)
 // #define PLAY_MODE() swim_wotf(SWIM_HS, 0x500F, 0x00)
@@ -612,12 +651,12 @@ uint8_t gb_write_page_buffer(uint8_t addrH, buffer* buff)
     // add word to write buffer
     gameboy_pin31_wr(addr, value);
 
-    cur++
+    cur++;
   }
   buff->cur_byte = cur;
 
   // write program buffer to flash (confirm)
-  gameboy_pin31_wr(addrH << 8, 0x29);
+  gameboy_pin31_wr(base_addr, 0x29);
 
   // TODO: add timeout
   do {
@@ -921,7 +960,9 @@ uint8_t flash_buff(buffer* buff)
         nes_write_page_verify((0x80 + addrH), buff, gtrom_prgrom_flash_wr);
       }
       if(buff->mapper == RNBW) {
-        if(buff->part_num == USE_UNLOCK_BYPASS) {
+        if(buff->part_num == USE_BUFFER) {
+          nes_cpu_write_page_buffer_verify((0x80 + addrH), buff);
+        } else if(buff->part_num == USE_UNLOCK_BYPASS) {
           // enter unlock mode bypass
           nes_cpu_wr(0x8AAA, 0xAA);
           nes_cpu_wr(0x8555, 0x55);
@@ -937,12 +978,11 @@ uint8_t flash_buff(buffer* buff)
           // reset the flash chip, supposed to exit too
           nes_cpu_wr(0x8000, 0xF0);
         } else {
-          // write data
           nes_write_page_verify((0x80 + addrH), buff, rnbw_prgrom_flash_wr);
         }
       }
       if(buff->mapper == VRC6a || buff->mapper == VRC6b) {
-        // write_page_verify((0x80 + addrH), buff, rnbw_prgrom_flash_wr);
+        // nes_write_page_verify((0x60 + addrH), buff, vrc6_prgrom_flash_wr);
         nes_write_page((0x60 + addrH), buff, vrc6_prgrom_flash_wr);
       }
       break;
@@ -966,16 +1006,16 @@ uint8_t flash_buff(buffer* buff)
       if(buff->mapper == CDREAM) {
         nes_write_page(addrH, buff, cdream_chrrom_flash_wr);
       }
-      if(buff->mapper == DPROM) {
-        // select bank
-        // 8KB banks $0000-1FFF
-        // page_num shift by 5 bits A13 >> A8(0)
-        bank = (buff->page_num) >> 5;
-        // write bank to register
-        nes_ppu_wr(0x3FFF, bank);
-        addrH &= 0x1F; // only A12-8 are directly addressable
-        nes_write_page_dualport(0, addrH, buff, nes_dualport_wr, nes_dualport_rd);
-      }
+      // if(buff->mapper == DPROM) {
+      //   // select bank
+      //   // 8KB banks $0000-1FFF
+      //   // page_num shift by 5 bits A13 >> A8(0)
+      //   bank = (buff->page_num) >> 5;
+      //   // write bank to register
+      //   nes_ppu_wr(0x3FFF, bank);
+      //   addrH &= 0x1F; // only A12-8 are directly addressable
+      //   nes_write_page_dualport(0, addrH, buff, nes_dualport_wr, nes_dualport_rd);
+      // }
       if(buff->mapper == VRC6a || buff->mapper == VRC6b) {
         // nes_write_page(addrH, buff, vrc6_chrrom_flash_wr);
         nes_write_page(addrH, buff, mmc3_chrrom_flash_wr);
@@ -1116,7 +1156,6 @@ uint8_t flash_buff(buffer* buff)
 
       if(buff->mapper == MBC1 || buff->mapper == MBC5) {
         if(buff->part_num == USE_BUFFER) {
-          // write data
           result = gb_write_page_buffer(addrH + 0x40, buff);
         } else if(buff->part_num == USE_UNLOCK_BYPASS) {
           // enter unlock bypass mode
@@ -1134,7 +1173,6 @@ uint8_t flash_buff(buffer* buff)
           // reset/exit
           gameboy_pin31_wr(0x0000, 0xF0);
         } else {
-          // write data
           gb_write_page_flash(addrH + 0x40, buff, gameboy_3v_flash_pin31_wr);
         }
       }
